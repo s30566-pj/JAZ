80ab89757f7b90eb2fa63a7c69f4e5b59baa34e4
s30566 <s30566@pjwstk.edu.pl>
2025-03-13 02:10:07 +0100

    Ćwiczenie 5

--- zajecia_1/src/main/java/pl/pjatk/danmak/components/One.java
+++ zajecia_1/src/main/java/pl/pjatk/danmak/components/One.java
@@ -1,10 +1,12 @@
 package pl.pjatk.danmak.components;
 
 
+import org.springframework.beans.factory.annotation.Value;
 
 public class One {
-    public One(){
+    public One(@Value("${my.custom.property}") String value){
         System.out.println("One");
+        System.out.println(value);
     }
 
     public void printMe(){
