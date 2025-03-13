2d8fed8f6be84505c86e8d34e41bc293c7dbdbd2
s30566 <s30566@pjwstk.edu.pl>
2025-03-13 02:18:36 +0100

    Ćwiczenie 6 i 7

--- zajecia_1/src/main/java/pl/pjatk/danmak/components/One.java
+++ zajecia_1/src/main/java/pl/pjatk/danmak/components/One.java
@@ -4,7 +4,7 @@ package pl.pjatk.danmak.components;
 import org.springframework.beans.factory.annotation.Value;
 
 public class One {
-    public One(@Value("${my.custom.property}") String value){
+    public One(@Value("${my.custom.property:nie ma i nie będzie}") String value){
         System.out.println("One");
         System.out.println(value);
     }
