b32b9619bbec7386100bae7f75acac641f0dd4b6
s30566 <s30566@pjwstk.edu.pl>
2025-03-29 11:18:34 +0100

    zjazd 3 zadanie 4

--- zajecia_1/src/main/java/pl/pjatk/danmak/controller/MessageControler.java
+++ zajecia_1/src/main/java/pl/pjatk/danmak/controller/MessageControler.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.pjatk.danmak.components.Car;
 import pl.pjatk.danmak.services.CarService;
@@ -26,8 +27,12 @@ public class MessageControler {
     }
 
     @GetMapping("/test/hello")
-    public ResponseEntity<String> hello(){
-        return ResponseEntity.ok("Hello world");
+    public ResponseEntity<String> hello(@RequestParam String reqParam){
+        if (reqParam != null){
+            return ResponseEntity.ok(reqParam);
+        } else {
+            return ResponseEntity.ok("Hello world");
+        }
 
     }
 
@@ -40,4 +45,5 @@ public class MessageControler {
     public ResponseEntity<String> someValue(@PathVariable("someValue") String wartosc){
         return ResponseEntity.ok(wartosc);
     }
+
 }
