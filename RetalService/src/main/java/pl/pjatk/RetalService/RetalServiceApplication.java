53893fb68c46300144979241289d905ff1314243
s30566 <s30566@pjwstk.edu.pl>
2025-05-24 10:57:01 +0200

    RentalService

---
+++ RetalService/src/main/java/pl/pjatk/RetalService/RetalServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.RetalService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetalServiceApplication.class, args);
+	}
+
+}
---
+++ RetalService/src/test/java/pl/pjatk/RetalService/RetalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.RetalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
